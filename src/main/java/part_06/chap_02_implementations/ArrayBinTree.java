c868a04dc211d04ffda33c388fab42e43ff322ca
vitalispopoff <vitalis.popoff@gmail.com>
2020-03-02 00:29:05 +0100

    wiki even further

---
+++ src/main/java/part_06/chap_02_implementations/ArrayBinTree.java
@@ -0,0 +1,15 @@
+package part_06.chap_02_implementations;
+
+public class ArrayBinTree {
+
+    static ArrayBinTree root = null;
+
+    ArrayBinTree
+            branches[] = {null, null},
+//            prev = null,
+            next = null;
+
+    ArrayBinTree() {
+        if (root == null) root = this;
+    }
+}
---
+++ src/main/java/part_06/chap_02_implementations/ListBinTree.java
@@ -0,0 +1,18 @@
+package part_06.chap_02_implementations;
+
+public class ListBinTree {
+
+    static ListBinTree
+            root = null,
+            lastLeaf = null;
+
+    ListBinTree
+            leftBranch = null,
+            rightBranch = null,
+            prev = null,
+            next = null;
+    String value = null;
+
+    ListBinTree() {
+        if (root == null) root = lastLeaf = this;}
+}
