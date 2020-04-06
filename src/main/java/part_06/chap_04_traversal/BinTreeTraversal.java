c28aacb4ae9c252fd15739defa0eea7432598dab
vitalispopoff <vitalis.popoff@gmail.com>
2020-04-06 02:35:16 +0200

    6.4.0 traverse code available

--- src/main/java/part_06/chap_04_traversal/BinTreeTraversal.java
+++ src/main/java/part_06/chap_04_traversal/BinTreeTraversal.java
@@ -1,22 +1,48 @@
 package part_06.chap_04_traversal;
 
+//  @formatter:off
 public class BinTreeTraversal {
 
+    int queueLength = 1;
     BinTreeTraversalNode
             root = null,        // * root of the tree ; declaring !static we allow to make subtrees to a later merge
             head = root,        // * head of the queue ; FIXME this one should be managed by reflection for cases of tree merging
             tail = root;        // * tail of the queue ; FIXME this one should be managed by reflection for cases of tree merging
 
-    //  @formatter:off
     void resetTraversal() {head = tail = root;}
-    //  @formatter:on
 
     void addNode(BinTreeTraversalNode node) {
         if (root == null) root = node;
         else root.addAsBranch(node);
     }
 
-//    void traverse(BinTreeTraversalNode node){}
+    BinTreeTraversalNode traverse() {
+        BinTreeTraversalNode result = null;
+
+        while (queueLength > 0) {
+            if (head.branchLeft != null) addToQueue(head.branchLeft);
+            if (head.branchRight != null) addToQueue(head.branchRight);
+            result = removeFromQueue();
+        }
+        return result;
+    }
+
+    void addToQueue(BinTreeTraversalNode node) {
+        tail.setNextInQueue(node);
+        tail = node;
+        queueLength++;
+    }
+
+    BinTreeTraversalNode removeFromQueue() {
+        BinTreeTraversalNode result = head;
+
+        if (queueLength > 1) {
+            head = head.nextInQueue;
+            result.removeFromQueue();
+            queueLength--;
+        }
+        return result;
+    }
 
     static class BinTreeTraversalNode {
 
@@ -27,13 +53,15 @@ public class BinTreeTraversal {
                 branchRight = null,
                 nextInQueue = null;
 
-        //    @formatter:off
         BinTreeTraversalNode(BinTreeTraversal tree){this.tree = tree;}
+
         void setTree(BinTreeTraversal tree) {this.tree = tree;}
+
         void setLeftBranch(BinTreeTraversalNode node) {this.branchLeft = node;}
         void setRightBranch(BinTreeTraversalNode node) {this.branchRight = node;}
+
         void setNextInQueue(BinTreeTraversalNode node) {this.nextInQueue = node;}
-        //    @formatter:on
+        void removeFromQueue() {nextInQueue = null;}
 
         void addAsBranch(BinTreeTraversalNode node) {
             if (branchLeft == null) branchLeft = node;
@@ -42,3 +70,4 @@ public class BinTreeTraversal {
         }
     }
 }
+//    @formatter:on
