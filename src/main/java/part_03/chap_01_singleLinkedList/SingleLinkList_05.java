11f84cc33af78e08378ef2c27cbb54dd5aedeb4b
vitalispopoff <vitalis.popoff@gmail.com>
2020-06-20 11:18:58 +0200

    decorator pattern based single linked list

---
+++ src/main/java/part_03/chap_01_singleLinkedList/SingleLinkList_05.java
@@ -0,0 +1,44 @@
+package part_03.chap_01_singleLinkedList;
+
+public class SingleLinkList_05 {
+
+    static int mainId = 0;
+    SingleLinkList_05 next = null;
+    int id;
+
+    SingleLinkList_05() {
+//        next = null;
+        setId();
+    }
+
+    SingleLinkList_05(SingleLinkList_05 next) {
+        this();
+        this.next = next;
+    }
+
+    public void removeNext() {
+        SingleLinkList_05 cache = next;
+        next = cache.next;
+        cache.next = null;
+    }
+
+    public SingleLinkList_05 getNext() {
+        return next;
+    }
+
+    void setId(){id = mainId++;}
+
+    public int getId() {return id;
+    }
+
+//  main method //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+
+    static SingleLinkList_05 list = new SingleLinkList_05();
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 2; i++) list = new SingleLinkList_05(list);
+
+        for (SingleLinkList_05 i = list; i != null; i = i.getNext()) System.out.println(i.getId());
+    }
+}
