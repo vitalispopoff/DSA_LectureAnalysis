8295f4719db2a13e95127d5c2c121839524a5c24
vitalispopoff <vitalis.popoff@gmail.com>
2020-07-14 13:30:37 +0200

    Merge branch 'master' of https://github.com/vitalispopoff/DSA_LectureAnalysis
    # Conflicts:
    #	src/main/java/part_06/chap_04_traversal/Structured.java

--- src/main/java/part_06/chap_04_traversal/BinTreeRecursiveTraversal.java
+++ src/main/java/part_06/chap_04_traversal/BinTreeRecursiveTraversal.java
@@ -1,39 +1,49 @@
+//    @formatter:off
+
 package part_06.chap_04_traversal;
 
-public class BinTreeRecursiveTraversal implements Structured, Traversing {
+public class BinTreeRecursiveTraversal implements Structured<BinTreeRecursiveTraversal>, Traversing {
 
-    Structured
+    private BinTreeRecursiveTraversal
             branchLeft,
             branchRight;
     int
             value;
 
-    public BinTreeRecursiveTraversal() {
-    }
+//  constructors
 
-    public BinTreeRecursiveTraversal(int value) {
+    public BinTreeRecursiveTraversal() { }
+
+/*    public BinTreeRecursiveTraversal(int value) {
         setValue(value);
-    }
+    }*/     //  overloaded constructor - currently unused
 
-    public BinTreeRecursiveTraversal(Structured left, Structured right, int value) {
+/*    public BinTreeRecursiveTraversal(BinTreeRecursiveTraversal left, BinTreeRecursiveTraversal right, int value) {
         setValue(value);
         setBranchLeft(left);
         setBranchRight(right);
+    }*/     //  overloaded constructor - currently unused
+
+//  overrides
+
+    @Override
+    public BinTreeRecursiveTraversal cloneIt(){
+        return new BinTreeRecursiveTraversal();
     }
 
     @Override
     public void traverse() { }  // TODO
 
-//  getters & setters \\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+//  getters & setters
 
-//    @formatter:off
 
-    public Structured getBranchLeft() { return branchLeft; }
-    public Structured getBranchRight() { return branchRight; }
+
+    public BinTreeRecursiveTraversal getBranchLeft() { return branchLeft; }
+    public BinTreeRecursiveTraversal getBranchRight() { return branchRight; }
     public int getValue() { return value;}
 
-    @Override public void setBranchLeft(Structured branchLeft) { this.branchLeft = branchLeft; }
-    @Override public void setBranchRight(Structured branchRight) { this.branchRight = branchRight; }
+    @Override public void setBranchLeft(BinTreeRecursiveTraversal branchLeft) { this.branchLeft = branchLeft; }
+    @Override public void setBranchRight(BinTreeRecursiveTraversal branchRight) { this.branchRight = branchRight; }
     @Override public void setValue(int value) { this.value = value; }
 
 //    @formatter:on
