90921911c3c3e256fcb5c1749a290096a00090fa
vitalispopoff <vitalis.popoff@gmail.com>
2020-07-14 13:30:52 +0200

    Merge branch 'master' of https://github.com/vitalispopoff/DSA_LectureAnalysis
    # Conflicts:
    #	src/main/java/part_06/chap_04_traversal/Structured.java

--- src/main/java/part_06/chap_04_traversal/Structured.java
+++ src/main/java/part_06/chap_04_traversal/Structured.java
@@ -4,63 +4,88 @@ import java.lang.Class;
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 
-public interface Structured {
+public interface Structured<T extends Structured<T>> {
 
-	abstract void setBranchLeft(Structured left);
+    void setBranchLeft(T left);
+    void setBranchRight(T right);
+    void setValue(int value);
+    int getValue();
 
-	abstract void setBranchRight(Structured right);
+    static <T extends Structured<T>> void makeGenericTree(T tree, int levels) {
+        ArrayList<T>
+                treeArray = new ArrayList<T>();
+        treeArray.add(tree);
 
-	abstract void setValue(int value);
+        for (int i = 1; i < 2 << levels; i++){
+            T
+                    temporal = tree.cloneIt();
 
-	abstract int getValue();
+            treeArray.add(i, temporal);
+            treeArray.get(i).setValue(i+1);
+        }
+        for (int i = 1; i <= 2<<(levels - 1); i++){
+            T
+                    node = treeArray.get(i-1),
+                    left = treeArray.get((i<<1) - 2),
+                    right = treeArray.get((i<<1) - 1);
+            node.setBranchLeft(left);
+            node.setBranchRight(right);
+        }
+    }
 
-	static Structured makeTree(Structured tree, int levels) {
-		ArrayList<Structured>
-				treeArray = new ArrayList<>();
-		treeArray.add(tree);
+    default <T extends Structured<T>> T cloneIt(){
+        return cloneIt((T)this);
+    }
 
-		for (int i = 1; i <= 2 << levels; i++) {
-			treeArray.add(i, cloneIt(tree));
-		}
-		for (int i = 1; i <= 2 << (levels - 1); i++) {
-			Structured
-					node = treeArray.get(i - 1),
-					left = treeArray.get((i << 1) - 2),
-					right = treeArray.get((i << 1) - 1);
-			node.setBranchLeft(left);
-			node.setBranchRight(right);
-		}
-		return tree;
-	}
+/*    static <T extends Structured<T>> void makeReflectionTree(T tree, int levels) {
+        ArrayList<T>
+                treeArray = new ArrayList<>();
+        treeArray.add(tree);
 
-	static Structured cloneIt(Structured object) {
-		String
-				objectClassName = object.getClass().toString().substring(6);
-		Class<?>
-				objectClass;
-		Constructor<?>
-				objectClassConstructor;
-		Object
-				clone;
+        for (int i = 1; i < 2 << levels; i++){
+            treeArray.add(i, cloneIt(tree));
+            treeArray.get(i).setValue(i+1);
+        }
+        for (int i = 1; i <= 2<<(levels - 1); i++){
+            T
+                    node = (T) treeArray.get(i-1),
+                    left = (T) treeArray.get((i<<1) - 2),
+                    right = (T) treeArray.get((i<<1) - 1);
+            node.setBranchLeft(left);
+            node.setBranchRight(right);
+        }
+    }*/     // makeTree disposable implementation
 
-		try {
-			objectClass = Class.forName(objectClassName);
-			objectClassConstructor = objectClass.getConstructor();
-			clone = objectClassConstructor.newInstance();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			clone = null;
-		}
+    static <T> T cloneIt(T object){
+        String
+                objectClassName = object.getClass().toString().substring(6);
+        Class<T>
+//                fields[] = {},
+                objectClass;
+        Constructor<T>
+                objectClassConstructor;
+        Object
+                clone;
 
-		return (Structured) clone;
-	}
+        try{
+            objectClass = (Class<T>) Class.forName(objectClassName);
+            objectClassConstructor = objectClass.getConstructor(/*fields*/);
+            clone = objectClassConstructor.newInstance(/*fields*/);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            clone = null;
+        }
 
-	static void main(String[] args) {
+        return (T) clone;
+    }
 
-		int levels = 3;
+    static void main(String[] args) {
 
-		BinTreeRecursiveTraversal
-				tree = (BinTreeRecursiveTraversal) makeTree(new BinTreeRecursiveTraversal(), levels);
-	}
+        BinTreeRecursiveTraversal tree = new BinTreeRecursiveTraversal();
+        makeGenericTree(tree, 3);
+
+        System.out.println(tree.getBranchRight().getBranchLeft().getValue());
+
+    }
 }
